195b709a8107bf3659bc905f8ed86dc0c3b91082
Lucas <lucas.selfslagh@gmail.com>
2020-10-17 12:34:58 +0200

    shader

---
+++ Assets/MoonSceneController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoonSceneController : MonoBehaviour
+{
+  public Globe Globe;
+
+  [Range(0.01f, .5f)]
+  public float Multiplier = .2f;
+
+  private Material _globeMat;
+  private float _pingPong;
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+    _globeMat = Globe.GetComponent<MeshRenderer>().sharedMaterial;
+  }
+
+  // Update is called once per frame
+  private void Update()
+  {
+    _globeMat.SetFloat("Opacity", Mathf.PingPong(Time.time * Multiplier, 1f));
+  }
+}
