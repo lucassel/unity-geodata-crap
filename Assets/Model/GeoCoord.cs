81525f32334e3af38e90d8b21c7d8a156ad171af
Lucas <lucas.selfslagh@gmail.com>
2020-10-25 20:41:12 +0100

    cleanup geocoordinate, refactor functional

--- Assets/Model/GeoCoord.cs
+++ Assets/Model/GeoCoord.cs
@@ -6,31 +6,23 @@
 public class GeoCoord
 {
   /// <summary>
-  /// 
+  /// WGS84 notation with single float accuracy gives about 11cm precision on terrestial scale.
   /// </summary>
-  public Vector2 polarCoordinate { get; private set; }
-  public Vector2 worldCoordinate { get; private set; }
+  public readonly Vector2 polarCoordinate;
+
   public Vector3 worldPosition { get; private set; }
   public Vector3 spherePosition { get; private set; }
-
-  private Vector3 CalculateCartesian(PlaneOrientation orientation) => orientation == PlaneOrientation.XY ? new Vector3(worldCoordinate.x, worldCoordinate.y, _elevation) : new Vector3(worldCoordinate.x, _elevation, worldCoordinate.y);
-
-  private float _elevation;
-
-  private Vector3 CalculateSpherical(float radius) => SphericalCoordinates.SphericalToCartesian(radius, polarCoordinate.x, polarCoordinate.y);
 
   public GeoCoord(Vector2 coord, PlaneOrientation orientation, float radius, float elevation)
   {
     polarCoordinate = coord;
-    _elevation = elevation;
-    UpdateCoordinate(orientation, radius, _elevation);
+    UpdateCoordinate(orientation, radius, elevation);
   }
 
   public void UpdateCoordinate(PlaneOrientation orientation, float radius, float elevation)
   {
-    worldCoordinate = new Vector2(polarCoordinate.x, polarCoordinate.y) * radius / 90f;
-    worldPosition = CalculateCartesian(orientation);
-    spherePosition = CalculateSpherical(radius + elevation);
-    _elevation = elevation;
+    Vector2 worldCoordinate = SphericalCoordinates.PolarCoordinatesToWorldCoordinates(polarCoordinate, radius);
+    worldPosition = SphericalCoordinates.CalculateCartesian(worldCoordinate, orientation, elevation);
+    spherePosition = SphericalCoordinates.SphericalToCartesian(radius + elevation, polarCoordinate.x, polarCoordinate.y);
   }
 }
--- Assets/Model/SphericalCoordinates.cs
+++ Assets/Model/SphericalCoordinates.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public static class SphericalCoordinates
 {
-  private static float ConvertToRadians(float angle) => (float)(Math.PI / 180) * angle;
-
+  /// <summary>
+  /// Converts WGS notation into Vector3 sphere position.
+  /// </summary>
+  /// <param name="radius"> </param>
+  /// <param name="polar"> </param>
+  /// <param name="elevation"> </param>
+  /// <returns> </returns>
   public static Vector3 SphericalToCartesian(float radius, float polar, float elevation)
   {
     polar = ConvertToRadians(polar);
@@ -16,4 +21,24 @@ public static class SphericalCoordinates
     outCart.z = a * Mathf.Sin(polar);
     return outCart;
   }
+
+  /// <summary>
+  /// Converts WGS84 Vector2 coordinate into a world-space Vector2, with a supplied globe radius.
+  /// </summary>
+  /// <param name="polar"> </param>
+  /// <param name="radius"> </param>
+  /// <returns> </returns>
+  public static Vector2 PolarCoordinatesToWorldCoordinates(Vector2 polar, float radius) => new Vector2(polar.x, polar.y) * radius / 90f;
+
+  /// <summary>
+  /// Converts a Vector2 world coordinate into a Vector3 world coordinate with a specific
+  /// orientation and elevation.
+  /// </summary>
+  /// <param name="worldCoordinate"> </param>
+  /// <param name="orientation"> </param>
+  /// <param name="elevation"> </param>
+  /// <returns> Vector3 world coordinate </returns>
+  public static Vector3 CalculateCartesian(Vector2 worldCoordinate, PlaneOrientation orientation, float elevation) => orientation == PlaneOrientation.XY ? new Vector3(worldCoordinate.x, worldCoordinate.y, elevation) : new Vector3(worldCoordinate.x, elevation, worldCoordinate.y);
+
+  private static float ConvertToRadians(float angle) => (float)(Math.PI / 180) * angle;
 }
